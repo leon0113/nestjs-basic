4fd7717dd4eb1ab6f391aafdddb6c8ea62b13107
Leon <tahjib.leon399@gmail.com>
2024-09-01 22:42:47 +0600

    nestjs controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PUT /users/:id
+    DELETE /users/:id
+    */
+
+    //? /users or /users?role=value
+    @Get()
+    findAll(@Query('role') role?: 'intern' | 'engineer' | 'admin') {
+        return role;
+    }
+
+    // // GET INTERN
+    // @Get('interns')
+    // getAllInterns() {
+    //     return [];
+    // }
+
+    // /GET /users/:id
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return { id };
+    }
+
+    //  POST /users
+    @Post()
+    create(@Body() userData: {}) {
+        return userData;
+    }
+
+    // PUT /users/:id
+    @Patch(':id')
+    updateOne(@Param('id') id: string, @Body() userUpdateData: {}) {
+        return { id, ...userUpdateData };
+    }
+
+
+    // /Delete /users/:id
+    @Delete(':id')
+    deleteOne(@Param('id') id: string) {
+        return { id };
+    }
+
+}
