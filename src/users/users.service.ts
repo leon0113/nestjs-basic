0e01dc2cb94b69f7bd3caf76d5c835fdde90c1d0
Leon <tahjib.leon399@gmail.com>
2024-09-01 23:53:50 +0600

    nestjs service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "email": "one@gmail.com",
+            "name": "One",
+            "role": "intern"
+        },
+        {
+            "id": 2,
+            "email": "two@gmail.com",
+            "name": "Two",
+            "role": "engineer"
+        },
+        {
+            "id": 3,
+            "email": "three@gmail.com",
+            "name": "Three",
+            "role": "admin"
+        },
+        {
+            "id": 4,
+            "email": "four@gmail.com",
+            "name": "Four",
+            "role": "intern"
+        },
+        {
+            "id": 5,
+            "email": "five@gmail.com",
+            "name": "Five",
+            "role": "engineer"
+        }
+    ];
+
+    findAll(role: 'intern' | 'engineer' | 'admin') {
+        if (role) {
+            return this.users.filter(user => user.role === role)
+        }
+        return this.users;
+    }
+
+}
